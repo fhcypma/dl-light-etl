8dc68c4ac7986d8f480c54689f2aa17de148af51
Frank Ypma <frank.ypma@ypma-data.nl>
2022-10-17 06:40:20 +0200

    Removed unused code. Improved type checks

--- dl_light_etl/jobs.py
+++ dl_light_etl/jobs.py
@@ -1,8 +1,5 @@
-import datetime
 import logging
-from typing import List
-
-from pyspark.sql import SparkSession
+from typing import List, Optional
 
 from dl_light_etl.actions import DoNothingAction, LogDurationAction, LogStartTimeAction
 from dl_light_etl.extractors import Extractors
@@ -11,42 +8,23 @@ from dl_light_etl.transformers import Transformers
 from dl_light_etl.types import AnyDataType, JobParameters
 
 
-class DailyJob:
-    def __init__(self, run_date: datetime.date) -> None:
-        assert type(run_date) == datetime.date
-
-        self.run_date = run_date
-
-
-class FrequentJob:
-    def __init__(self, run_datetime: datetime.datetime) -> None:
-        assert type(run_datetime) == datetime.datetime
-
-        self.run_datetime = run_datetime
-
-
-class SparkDrivenJob:
-    def __init__(self) -> None:
-        self.spark = SparkSession.Builder.getOrCreate()
-
-
 class EtlJob:
     def __init__(
         self,
         *,
-        pre_extractor_action: DoNothingAction = None,
+        pre_extractor_action: Optional[DoNothingAction] = None,
         extractors: Extractors,
-        post_extractor_action: DoNothingAction = None,
-        transformers: Transformers = None,
-        pre_loader_action: DoNothingAction = None,
+        post_extractor_action: Optional[DoNothingAction] = None,
+        transformers: Optional[Transformers] = None,
+        pre_loader_action: Optional[DoNothingAction] = None,
         loader: AbstractLoader,
-        post_loader_action: DoNothingAction = None,
+        post_loader_action: Optional[DoNothingAction] = None,
     ) -> None:
         self.pre_extractor_action = (
             pre_extractor_action if pre_extractor_action else LogStartTimeAction()
         )
 
-        assert type(extractors) == Extractors
+        assert isinstance(extractors, Extractors)
         assert len(extractors.extractors) > 0, "No extractor defined for job"
         self.extractors = extractors
 
@@ -61,7 +39,7 @@ class EtlJob:
             pre_loader_action if pre_loader_action else DoNothingAction()
         )
 
-        assert issubclass(type(loader), AbstractLoader)
+        assert isinstance(loader, AbstractLoader)
         self.loader = loader
 
         self.post_loader_action = (
@@ -81,26 +59,3 @@ class EtlJob:
         parameters = self.pre_loader_action.execute(parameters, data)
         parameters = self.loader.load(parameters, data)
         parameters = self.post_loader_action.execute(parameters, data)
-
-
-class SparkJob(EtlJob):
-    def __init__(
-        self,
-        *,
-        pre_extractor_action: DoNothingAction = None,
-        extractors: Extractors,
-        post_extractor_action: DoNothingAction = None,
-        transformers: Transformers = None,
-        pre_loader_action: DoNothingAction = None,
-        loader: AbstractLoader,
-        post_loader_action: DoNothingAction = None,
-    ) -> None:
-        super().__init__(
-            pre_extractor_action=pre_extractor_action,
-            extractors=extractors,
-            post_extractor_action=post_extractor_action,
-            transformers=transformers,
-            pre_loader_action=pre_loader_action,
-            loader=loader,
-            post_loader_action=post_loader_action,
-        )
